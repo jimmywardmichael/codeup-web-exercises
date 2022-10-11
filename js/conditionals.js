950784506dbdab58fc5c14b1b7d54e57cf2583f8
James Ward <jimmywardmichael@gmail.com>
2022-10-11 14:31:54 -0500

    finilized conditional

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,5 +1,5 @@
 "use strict";
-(function () {
+(function(){
     /* ########################################################################## */
 
     /**
@@ -21,16 +21,28 @@
      * Test your function by passing various string literals to it and
      * console.logging the function's return value
      */
+    // 1. Function name analyzeColor/
+        // 2. Establish a parameter named 'color'
+        // 3. Need to return a string
+        // 4. Return a string depending on input that is passed.
 
-    let analyzeColor = prompt("What is your favorite color?")
-    if (analyzeColor.toLowerCase() === 'blue') {
-        console.log("blue is the color of the sky!");
-    } else if (analyzeColor.toLowerCase() === 'red') {
-        console.log("Strawberries are red!");
-    } else if (analyzeColor.toLowerCase() === 'cyan') {
-        console.log("I don't know anything about cyan!");
-    }
-    ;
+    function analyzeColor(color) {
+        //return some string
+        if(color === 'red') {
+            return 'Red is good';
+        } else if (color === 'blue') {
+            return 'blue is better than red';
+        } else if (color === 'yellow'){
+            return 'Yellow is the best choice'
+        } else {
+            return 'Never heard of that one, that color is no good'
+        }
+    };
+    console.log(analyzeColor('blue'));
+    console.log(analyzeColor('red'));
+    console.log(analyzeColor('cyan'));
+    console.log(analyzeColor('yellow'));
+    console.log(analyzeColor('marigold'));
 
 
 // Don't change the next two lines!
@@ -45,44 +57,39 @@
 //  * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
 //  * You should see a different message every time you refresh the page
 //  */
-    if (randomColor.toLowerCase) {
-        console.log(randomColor + " is a nice choice!");
-    }
-    ;
+console.log(analyzeColor(randomColor));
 
-
-    /* TODO:
+     /* TODO:
     * Comment out the code above, and refactor your function to use a switch-case statement
     */
-    let colorPreference = prompt("What is your favorite color in the rainbow?");
+//return will exit out of code no break needed after it
 
-    switch (colorPreference) {
-        case "blue":
-            alert("What a coincidence, that's my favorite!");
-            break;
-        case "red":
-            alert("red is a great color");
-            break;
-        default:
-            alert(colorPreference + " isn't my favorite, but thats okay!");
-            break;
-    };
+    function analyzeColor(color){
+        switch (color) {
+            case 'red':
+                return 'Red is good';
+            case 'blue':
+                return 'Blue is better choice';
+            case 'yellow':
+                return 'Yellow is the best choice'
+            default:
+                return 'Never heard of that one, that color is no good';
+        }
+    }
+    console.log(analyzeColor('blue'));
+    console.log(analyzeColor('red'));
+    console.log(analyzeColor('cyan'));
+    console.log(analyzeColor('yellow'));
+    console.log(analyzeColor('marigold'));
+
     /**
      * TODO:
      * Prompt the user for a color when the page loads, and pass the input from the
      * user to your `analyzeColor` function. Alert the return value from your
      * function to show it to the user.
      */
-    let colorDecision = prompt("What is your favorite color choice?")
-    if (colorDecision.toLowerCase() === 'red') {
-        alert(analyzeColor)
-    } else if (colorDecision.toLowerCase() === 'blue') {
-        alert(analyzeColor)
-    } else if (colorDecision.toLowerCase() === 'cyan') {
-        alert(analyzeColor)
-    }
-    ;
 
+    alert(analyzeColor(prompt("Choose a color!")));
 
 /* TODO:
         * Suppose there's a promotion in Walmart, each customer is given a randomly
@@ -99,25 +106,48 @@
     * calculateTotal(4, 100) // returns 50
     * /calculateTotal(5, 100) // returns 0
 */
-    var luckyNumber = Math.floor(Math.random() * 5);
-    let totalAmount = prompt("How much is your total?")
-    if (luckyNumber * totalAmount){
-        alert("Your discounted price is " + luckyNumber);
-    };
+    // 1. function named calculateTotal
+    // 2. establish two paramters , one for luckyNum, one for totalAmt
+    // 3. return discPrice
+    // 4. Discounts
+        // 0 = no discount
+        // 1 = 0.1
+        // 2 = 0.25
+        // 3 = 0.35
+        // 4 = 0.5
+        // 5 = 1
+    // Depending on luckyNumber, a discount is applied!!!
 
+    function calculateTotal(luckyNum, totalAmount) {
+        if(luckyNum === 0){
+            return totalAmount;
+        } else if(luckyNum === 1) {
+            return totalAmount - (0.10 * totalAmount);
+        } else if(luckyNum === 2){
+            return totalAmount - (0.25 * totalAmount);
+        } else if(luckyNum === 3){
+            return totalAmount - (0.35 * totalAmount);
+        } else if(luckyNum === 4){
+            return  totalAmount - (0.5 * totalAmount);
+        } else if(luckyNum === 5){
+            return 0;
+        }
+    }
+    console.log(calculateTotal(0, 100));// returns 100
+    console.log(calculateTotal(4, 100));// returns 100
+    console.log(calculateTotal(5, 100));// returns 100
 
-
-     // * TODO:
+  // * TODO:
      // * Uncomment the line below to generate a random number between 0 and 5.
      // * (In this line of code, 0 is inclusive, and 6 is exclusive)
      // * Prompt the user for their total bill, then use your `calculateTotal` function
      // * and alerts to display to the user what their lucky number was, what their
      // * price before the discount was, and what their price after the discount is.
      // */
-// Generate a random number between 0 and 6
- var luckyNumber1 = Math.floor(Math.random() * 6);
- let Totalbill = prompt("How much was your total bill?");
- alert calculateTotal("Your lucky number is " + luckyNumber1 + "price before discount " + discountBeforePrice + "price after discount " + discountAfterPrice);
+     // Generate a random number between 0 and 6
+    var luckyNumber = Math.floor(Math.random() * 6);
+    let userBill = prompt("What was the total amount of your bill?");
+    alert("Your lucky number is: " + luckyNumber + ". Your total before discount $" + userBill + '. Your total after the discount: $' + calculateTotal(luckyNumber, userBill));
 
     /**
      * TODO:
@@ -137,18 +167,28 @@
      * Can you refactor your code to use functions?
      * HINT: The way we prompt for a value could be improved
      */
-    if (prompt(confirm("Would you like to enter a number?"))){
-        prompt("Please enter a number")
-    } else if(){
-        alert("The number was even")
-    } else if(){
-    alert("The number was odd")
-    } else if(){
-        alert("The number was positive")
-    } else if(){
-        alert("The number was negativw")
-    } else if(){
-        alert("The number is " + 100;)
-    };
+    // 1. need to use a confirm()
+    // 2. need to use a prompt()
+    // 3. needs to be a number datatype input from user must be a number
+        //if it is:
+            // alert if even or odd
+            // alert input + 100
+            // alert positive or negative
+        //if it is not:
+            // alert user incorrect data type
+    // 4. If possible, refactor to function
+
+    let doesUserConfirm = confirm("Do you want to play a game?")
+    //confirm built in method will always pass a true or false boolean
+    if (doesUserConfirm) {
+        let userNum = parseFloat(prompt("Pick a number!"));
+        if (!isNaN(userNum)){
+            alert(userNum % 2 === 0 ? "Number is even" : "Number is odd. ")
+            alert(userNum > 0 ? "Number is positive " : "Number is negative.")
+            alert("Your number + 100 = " + (userNum + 100))
+        } else {
+            alert("Sorry, what you entered is not a number!")
+        }
+    }
 
 }());
