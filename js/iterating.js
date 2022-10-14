f4086708b8f52ef477704a09729ba72f786c1c09
James Ward <jimmywardmichael@gmail.com>
2022-10-14 15:36:31 -0500

    final commit on arrays

--- js/iterating.js
+++ js/iterating.js
@@ -13,16 +13,17 @@ let peoplesName = ['John', 'Jack', 'Jill', 'Jane'];
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log(peoplesName[0]);
-    console.log(peoplesName[1]);
-    console.log(peoplesName[2]);
-    console.log(peoplesName[3]);
+    console.log(peoplesName.length);
+
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    console.log(peoplesName.length);
+    console.log(peoplesName[0]);
+    console.log(peoplesName[1]);
+    console.log(peoplesName[2]);
+    console.log(peoplesName[3]);
 
     /**
      * TODO:
@@ -53,8 +54,8 @@ peoplesName.forEach(function (name ){
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+console.log(peoplesName[0]);
 console.log(peoplesName[1]);
 console.log(peoplesName[2]);
-console.log(peoplesName[3]);
 
 })();
