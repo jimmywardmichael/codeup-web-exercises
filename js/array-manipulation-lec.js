f2b5870c52772509bb06deb31810761897c7ebb0
James Ward <jimmywardmichael@gmail.com>
2022-10-17 10:49:15 -0500

    array lec update

---
+++ js/array-manipulation-lec.js
@@ -0,0 +1,28 @@
+let nums = [7, 3, 4, 1, 5, 6, 7, 2, 9, 10, 14, 11, 15, 23, 34]
+// nums.push(11, 12, 13)
+// nums.unshift(-2, 0, -1)
+// nums.pop();
+// nums.shift();
+// console.log(nums)
+
+// let nameArr = ['jim', 'joe', 'jim', 'john', 'jim', 'bob'];
+
+// console.log(nameArr.indexOf('john'));
+// console.log(nameArr.indexOf('jim'));
+// console.log(nameArr.lastIndexOf('jim'));
+
+// console.log(nameArr.slice(2, 3));
+// console.log(nameArr)
+// have to reassign variable to show new value is otherwise nameArr stays old value
+
+// console.log(nameArr.reverse());
+// console.log(nameArr[0])
+// //using the .reverse will actually reassign the array .
+// nameArr.sort()
+// console.log(nameArr)
+//  .sort sorts alphabetically
+//
+// let nameString = "jim, joe, jim, john, jim, bob"
+// console.log(nameString.split(","))
+nums.sort()
+console.log(nums)
