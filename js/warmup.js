730aafdd6eb91d42d652e9fe0dcdd1c2bb4f917a
James Ward <jimmywardmichael@gmail.com>
2022-10-20 09:36:21 -0500

    warmup

--- js/warmup.js
+++ js/warmup.js
@@ -27,16 +27,49 @@
     // console.log((multFive('five')));
     // console.log((multFive(true)));
 
-    let arr = [true, "Icon", 25, "66", false, 0];
-// arr.forEach(function (typePrinter){
-//     console.log(typePrinter);
+//     let arr = [true, "Icon", 25, "66", false, 0];
+// // arr.forEach(function (typePrinter){
+// //     console.log(typePrinter);
+// //
+//     function typePrinter(userArr){
+//         for(let i = 0; i < userArr.length; i++){
+//             console.log(typeof userArr[i])
+//         }
+//     }
+//     typePrinter(arr);
+//     Warm-Up:
 //
-    function typePrinter(userArr){
-        for(let i = 0; i < userArr.length; i++){
-            console.log(typeof userArr[i])
-        }
+//     Create a function named "average" that takes in an array of numeric values, and returns the average. If any of the elements in the array are non-numeric, the function should return false.
+//
+//         Ex:
+//     average(["6", 5, 3, 2, 9])                       // returns 5
+//     average([true, 6, 9, 3, 10])                     // returns false
+//     average([{name: "Codey the Duck}, 10, 3, false]) // returns false
+//         average([1, 2, 3, 4, 5])                         // returns 3
+//         average(["5.5", "five", 3, 6, 7])                // returns false
+    function isNumeric(input){
+        return !isNaN(parseFloat(input))
     }
-    typePrinter(arr);
+
+  function average(array){
+      let i=0;
+      let sum = 0;
+      while(i < array.length) {
+          if (isNumeric(array[i])) {
+              sum += parseFloat(array[i]);
+              i++;
+          } else {
+              return false;
+          }
+      }
+      return sum / array.length
+  }
+
+    console.log(average(["6", 5, 3, 2, 9]));
+    console.log(average([true, 6, 9, 3, 10]));
+    console.log(average(["Codey the Duck", 10, 3, false]));
+    console.log(average([1, 2, 3, 4, 5]));
+    console.log(average(["5.5", "five", 3, 6, 7]));
 })();
 
 
