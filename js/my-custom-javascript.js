a1521b9d3b44acd7289b91fc987059dbe39fd05e
James Ward <jimmywardmichael@gmail.com>
2022-11-16 11:21:05 -0600

    done

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,19 @@
 "use strict";
+(function(){
+    // $(function(){
+    //     alert('DOM has finished loading');
+    // })
+//$('selector').html("value changed to")
+//example: $('h2').html("I changed it");
 
-window.onload = function() {
-    alert( 'Dom has finished loading!' );
-}
+    var contents = $('#main').html();
+    alert(contents);
+    var contents1 = $('#li1').html();
+    alert(contents1);
+    $('.codeup').css('border', 'solid 1px red')
+    $('li1').css('font-size', '20px')
+    $('li2').css('font-size', '20px')
+    $('h1main').css('background-color', 'yellow')
+    $('p').css('background-color', 'yellow')
+
+})();
