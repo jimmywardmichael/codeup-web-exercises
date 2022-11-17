bd8d785e77bec1768e2ed4dbe648fdce6a249c72
James Ward <jimmywardmichael@gmail.com>
2022-11-17 09:18:54 -0600

    changes to my custom javascript

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,35 +1,36 @@
-"use strict";
-(function(){
-    $(function(){
-        alert('DOM has finished loading');
-    })
-$('selector').html("value changed to")
-example: $('h2').html("I changed it");
 
-    var contents = $('#main').html();
-    alert(contents);
-    var contents1 = $('#li1').html();
-    alert(contents1);
-    $('.codeup').css('border', 'solid 1px red')
-    $('li').css('font-size', '20px')
+    "use strict";
+    (function(){
+        $(function(){
+            alert('DOM has finished loading');
+        })
+        $('selector').html("value changed to")
+        example: $('h2').html("I changed it");
 
-    $('h1main').css('background-color', 'yellow')
-    $('p').css('background-color', 'yellow')
-    $('#h1main').click(
-        function() {
-            $(this).css('background-color', '#FF0');
-        }
-    );
-    $('#para').dblclick(function(e) {
-        $(this).css('font-size', '18px');
-    });
-    $('li').hover(
-        function() {
-            $(this).css('color', 'red');
-        },
-        function() {
-            $(this).css('color', 'black');
-        }
-    );
+        var contents = $('#main').html();
+        alert(contents);
+        var contents1 = $('#li1').html();
+        alert(contents1);
+        $('.codeup').css('border', 'solid 1px red')
+        $('li').css('font-size', '20px')
 
-})();
+        $('h1main').css('background-color', 'yellow')
+        $('p').css('background-color', 'yellow')
+        $('#h1main').click(
+            function() {
+                $(this).css('background-color', '#FF0');
+            }
+        );
+        $('#para').dblclick(function(e) {
+            $(this).css('font-size', '18px');
+        });
+        $('li').hover(
+            function() {
+                $(this).css('color', 'red');
+            },
+            function() {
+                $(this).css('color', 'black');
+            }
+        );
+
+    })();
