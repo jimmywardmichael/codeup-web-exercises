81c62e7a723cfa4d62bcbab8b214f2059a8eca29
James Ward <jimmywardmichael@gmail.com>
2022-11-29 11:04:44 -0600

    fixed issue with weather key

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,1 +1,6 @@
 "use strict"
+// $.get("http://api.openweathermap.org/data/2.5/forecast", {
+//     APPID: weatherBox_Token,
+//     q:     "San Antonio, US",
+//     units: "Imperial"
+// });
--- js/weathermap_lecture_js.js
+++ js/weathermap_lecture_js.js
@@ -1,15 +1,8 @@
 $.get("http://api.openweathermap.org/data/2.5/forecast", {
     APPID: OPEN_WEATHER_APPID,
-    q:     "San Antonio, US",
+    lat:    29.423017,
+    lon:   -98.48527,
     units: "imperial"
 }).done(function(data) {
-    console.log(data); // log all of JSON object
-    for(let i = 0; i <= 39;  i+=8){
-        console.log(data.list[i].main.temp_max);
-    };
-    // console.log(data.list[0].main.temp_max); //  pick out max temp - day 1
-    // console.log(data.list[8].main.temp_max); // day 2
-    // console.log(data.list[16].main.temp_max); // day 3
-    // console.log(data.list[24].main.temp_max); // day 4
-    // console.log(data.list[32].main.temp_max); // day 5
+    console.log('5 day forecast', data);
 });
