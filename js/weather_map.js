30fdb6f39819506d995f60f08ab19c7d99ba24ce
James Ward <jimmywardmichael@gmail.com>
2022-11-29 11:09:08 -0600

    moved js script over to js file

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,8 @@
 "use strict"
-// $.get("http://api.openweathermap.org/data/2.5/forecast", {
-//     APPID: weatherBox_Token,
-//     q:     "San Antonio, US",
-//     units: "Imperial"
-// });
+$.get("http://api.openweathermap.org/data/2.5/forecast", {
+    APPID: weatherBox_Token,
+    q:     "San Antonio, US",
+    units: "Imperial"
+}).done(function (data){
+    console.log(data)
+});
