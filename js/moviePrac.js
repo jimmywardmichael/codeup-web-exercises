d0c1449037fd801113765ef9c23227d710f5d6fd
James Ward <jimmywardmichael@gmail.com>
2022-12-06 21:24:44 -0600

    added imgs

--- js/moviePrac.js
+++ js/moviePrac.js
@@ -10,3 +10,30 @@ getMovies().then( movies => {
     })
 }).catch(error => console.log(error));
 //-------------------------------------------------------------grabs movies from db.json -----------------------------------------------------------------------//
+function simpleCard(movie){
+    let output = `
+            <div class="movie-card card">
+                <div class="card-body m-3">
+                    <div class="card-img-top">
+                        <img src="img/transformers.jpg" alt="Movie Poster">
+                    </div>
+                    Title: ${movie.title} <br>
+                    Rating: ${movie.rating} <br>
+                    Description: ${movie.plot} <br>
+                    Genre: ${movie.genre}<br>
+                    Year: ${movie.year}<br>
+                    Director: ${movie.director}<br>
+                    Actors/Actresses: ${movie.actors}<br>
+                    ID: ${movie.id}<br>
+                </div>
+                <div class="card-footer">
+                    <button>Delete</button>
+                    <button class="edit-btn">Edit</button>
+                </div>
+            </div>
+            ${addJSButtons()}
+          
+    `
+
+    console.log(output)
+}
