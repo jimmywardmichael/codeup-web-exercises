2094c0253365db32ab002b3f4930da467cb9ed66
James Ward <jimmywardmichael@gmail.com>
2022-12-07 09:49:47 -0600

    added loading function

--- js/moviePrac.js
+++ js/moviePrac.js
@@ -1,4 +1,25 @@
 "use strict";
+////////////////////// Loading Function///////////////////
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+        if (document.getElementsByTagName('html')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+        }
+    }, 3000);
+}
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+})
+
+
+
 //This promise below takes in a js object store it in a variable and returns function. This allows AJAX to talk to Glitch DB
 function getMovies (){
     return fetch('https://autumn-daffodil-ink.glitch.me/movies')
@@ -10,6 +31,9 @@ getMovies().then( movies => {
     })
 }).catch(error => console.log(error));
 //-------------------------------------------------------------grabs movies from db.json -----------------------------------------------------------------------//
+
+
+
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
